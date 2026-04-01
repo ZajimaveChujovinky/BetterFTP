bcb299e22603b1039141211352de37685204716b
Johny-Butcher <zednikhonza4@gmail.com>
2026-04-01 23:40:39 +0200

    added quota limits

--- src/core/services/AuthService.ts
+++ src/core/services/AuthService.ts
@@ -9,25 +9,30 @@ export class AuthService implements IAuthProvider {
     private repo: IUserRepository,
     private hasher: IPasswordHasher,
     private logger: ILogger
-  ) {}
+  ) { }
 
   async validateUser(username: string, passwordInput: string): Promise<FtpUser | null> {
     this.logger?.debug?.(`Validating user`, { username });
     // 1. Find user (from anywhere - JSON, SQL...)
     const user = await this.repo.findUser(username);
-    
+
     if (!user) {
-        this.logger?.debug?.(`User not found in repository`, { username });
-        return null;
+      this.logger?.debug?.(`User not found in repository`, { username });
+      return null;
     }
 
     const isValid = await this.hasher.verify(passwordInput, user.passwordHash);
 
     if (isValid) {
       this.logger?.debug?.(`Password valid`, { username });
-      return { username: user.username, homeDir: user.homeDir }; // Returning root directory
+      return {
+        username: user.username,
+        homeDir: user.homeDir,
+        maxFileSize: user.maxFileSize,
+        maxTotalSize: user.maxTotalSize,
+      };
     }
-    
+
     this.logger?.debug?.(`Password invalid`, { username });
     return null;
   }
